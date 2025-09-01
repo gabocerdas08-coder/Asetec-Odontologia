aaa5bc365480f6110df8bfc6be5f35fd82a0246a
gabocerdas08-coder <gabocerdas08@gmail.com>
2025-09-01 16:01:28 -0600

    Integración kinsta 1

---
+++ asetec-odontologia/asetec-odontologia.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: ASETEC Odontología
+ * Description: Módulo de citas/gestión para Odontología ASETEC.
+ * Version: 1.0.0
+ * Author: ASETEC TI
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Text Domain: asetec-odontologia
+ */
+if (!defined('ABSPATH')) exit; // seguridad básica

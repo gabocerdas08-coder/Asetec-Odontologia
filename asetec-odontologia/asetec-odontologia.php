e87fb2177343189a82cedf7c31e34546f9549e51
gabocerdas08-coder <gabocerdas08@gmail.com>
2025-09-11 10:13:18 -0600

    Mejoras calnedario 38

--- asetec-odontologia/asetec-odontologia.php
+++ asetec-odontologia/asetec-odontologia.php
@@ -9,7 +9,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 final class ASETEC_Odontologia {
-    const VERSION = '0.3.11';
+    const VERSION = '0.3.12';
     private static $instance = null;
 
     /** Feature flags (puedes activar más adelante) */
